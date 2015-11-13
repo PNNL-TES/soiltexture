cff6e652b0a32f087788fd626b70e2712625991a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-11-12 22:29:25 -0500

    Added a loop and a couple things

---
+++ soilgrids1km/README.md
@@ -0,0 +1,1 @@
+Data downloaded from ftp://soilgrids@ftp.soilgrids.org/data/recent/ on 12 November 2015.
--- test.R
+++ test.R
@@ -1,29 +1,40 @@
 
 
+library(R.utils)
+library(raster)
+
+
 # Load SRDB lon/lat values
 
 
 # Load the soilgrid1km files one by one
 SOILSDATA <- "~/Data/soilgrids1km/"
 
-fn <- "OCSTHA_M_02_apr_2014.tif.gz"
-fqfn <- file.path(SOILSDATA, fn)
+files <- list.files(SOILSDATA, pattern = "*.tif.gz")
 
-# get variable name from filename
+for(fn in files) {
+  
+  #fn <- "BLD_sd1_M_02_apr_2014.tif.gz"
+  fqfn <- file.path(SOILSDATA, fn)
+  
+  # get variable name from filename
+  varname <- paste(unlist(strsplit(fn, "_"))[1:3], collapse="_")
+  
+  
+  # gunzip the file to a temporary file
+  tf <- tempfile()
+  gunzip(fqfn, destname = tf)
+  
+  # load into raster
+  
+  d <- raster(tf)
+  print(d)
+  plot(d)
+  
+  # Use raster::extract(raster object, point list)
+  
+  file.remove(tf)
+  
+}
 
-library(R.utils)
-library(raster)
-
-# gunzip the file to a temporary file
-tf <- tempfile()
-gunzip(fqfn, destname = tf)
-
-# load into raster
-
-d <- raster(tf)
-plot(d)
-
-# Use raster::extract(raster object, point list)
-
-file.remove(tf)
 
